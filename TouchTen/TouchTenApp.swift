ccc41a10245e63604a10d44dcf3404eeea5a4932
IHakoda <Hakoda.AK@gmail.com>
2021-08-18 17:22:41 +0300

    Initial Commit

---
+++ TouchTen/TouchTenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchTenApp.swift
+//  TouchTen
+//
+//  Created by mr. Hakoda on 18.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchTenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
